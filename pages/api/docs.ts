f56f93c2998775e6d2fc0d8581742fe88d27296f
Greta Millar <gretamillar@pauls-air.lan>
2023-06-18 15:43:35 -0500

    change landing page to search page

--- pages/api/docs.ts
+++ pages/api/docs.ts
@@ -39,7 +39,7 @@ const handler = async (req: Request): Promise<Response> => {
   // console.log("input: ", input);
 
   const apiKey = process.env.OPENAI_API_KEY;
-  
+
   const apiURL = process.env.OPENAI_PROXY == "" ? "https://api.openai.com" : process.env.OPENAI_PROXY;
 
   const embeddingResponse = await fetch(
@@ -98,39 +98,39 @@ const handler = async (req: Request): Promise<Response> => {
 
   // console.log("contextText: ", contextText);
 
-  const systemContent = `You are a helpful assistant. When given CONTEXT you answer questions using only that information,
-  and you always format your output in markdown. You include code snippets if relevant. If you are unsure and the answer
-  is not explicitly written in the CONTEXT provided, you say
-  "Sorry, I don't know how to help with that."  If the CONTEXT includes 
-  source URLs include them under a SOURCES heading at the end of your response. Always include all of the relevant source urls 
+  const systemContent = `You are a helpful assistant with the voice of Taylor Swift. When given CONTEXT you answer questions using only that information or your knowledge of Taylor Swift,
+  and you always format your output in markdown. If you are unsure and the answer
+  is not explicitly written in the CONTEXT provided or in your knowledge of Taylor Swift, you say
+  "Sorry, we are never getting back together"  If the CONTEXT includes 
+  source URLs include them under a SOURCES heading at the end of your response. Always include all of the relevant source urls
   from the CONTEXT, but never list a URL more than once (ignore trailing forward slashes when comparing for uniqueness). Never include URLs that are not in the CONTEXT sections. Never make up URLs`;
 
   const userContent = `CONTEXT:
   Next.js is a React framework for creating production-ready web applications. It provides a variety of methods for fetching data, a built-in router, and a Next.js Compiler for transforming and minifying JavaScript code. It also includes a built-in Image Component and Automatic Image Optimization for resizing, optimizing, and serving images in modern formats.
   SOURCE: nextjs.org/docs/faq
-  
-  QUESTION: 
-  what is nextjs?    
+
+  QUESTION:
+  what is nextjs?
   `;
 
   const assistantContent = `Next.js is a framework for building production-ready web applications using React. It offers various data fetching options, comes equipped with an integrated router, and features a Next.js compiler for transforming and minifying JavaScript. Additionally, it has an inbuilt Image Component and Automatic Image Optimization that helps resize, optimize, and deliver images in modern formats.
-  
+
   \`\`\`js
   function HomePage() {
     return <div>Welcome to Next.js!</div>
   }
-  
+
   export default HomePage
   \`\`\`
-  
+
   SOURCES:
   https://nextjs.org/docs/faq`;
 
   const userMessage = `CONTEXT:
   ${contextText}
-  
-  USER QUESTION: 
-  ${query}  
+
+  USER QUESTION:
+  ${query}
   `;
 
   const messages = [
---
+++ pages/indexold.tsx
@@ -0,0 +1,24 @@
+// pages/index.tsx
+
+import { NextPage } from "next";
+import Link from "next/link";
+
+const HomePage: NextPage = () => {
+  return (
+    <div className="flex flex-col justify-center min-h-screen antialiased text-center text-slate-50">
+      <div className="flex flex-col items-center px-6 m-auto mx-auto max-w-7xl">
+        <h1 className="max-w-lg text-xl font-extrabold">
+          GRETA'S Chat GPT-3 Starter App
+        </h1>
+        <ul className="mt-10 mb-6 text-xs sm:text-sm text-slate-400">
+          <li><Link href="/embeddings" >Create Embeddings with URLs</Link></li>
+          <li><Link href="/embeddings2" >Create Embeddings by pasting</Link></li>
+          <li><Link href="/docs" >Search</Link></li>
+
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
